dc7faa93e81bf884b289c873cdce5777e1a94068
Danareynolds-coding <dana.reynolds@mscoding.org>
2025-07-19 16:22:18 -0500

    update

--- thingsToDo/src/components/ThingsToDo.jsx
+++ thingsToDo/src/components/ThingsToDo.jsx
@@ -1,30 +1,96 @@
-// import { useState } from "react";
+import { useState } from "react";
 import todoData from "../todoData";
 
 
 function ThingsToDo (){
+const [thingsToDo, setThingsToDo] = useState(todoData);
+    const [newThingsToDoName, setNewThingsToDoName] = useState("");
+    const [newThingsToDoDiscription, setNewThingsToDoDiscription] = useState("");
+    const [newThingsToDoTimeDue, setNewThingsToDoTimeDue] = useState("");
+    const [newThingsToDoDone, setNewThingsToDoDone] = useState(false);
 
-    // const [thingsToDo, setThingsToDo] = useState([])
+    function handleNameChange(event) {
+        setNewThingsToDoName(event.target.value);
+    }   
     
-    return (
-        
-       <div className="row">
-            <div className="col-12">
-                <h2  >To Do List</h2>
-                <hr></hr>
-                <ol className="col-12">
+    function handleDescriptionChange(event) {
+        setNewThingsToDoDiscription(event.target.value);
+    }
 
-                    {todoData.map(list => (
-                        <li  key={list.id} style={{ textDecoration: list.done ? 'line-through' : 'none' }}>
-                            <h5>{list.name}</h5>
-                            <p>{list.description}{" "}--{" "}{list.timeDue}</p>
-                            
-                        </li>
-                    ))}
-                </ol>
-            </div>
-            
+    function handleTimeDueChange(event) {
+        setNewThingsToDoTimeDue(event.target.value);
+    }
+    
+    function addTask() {
+        if (
+            newThingsToDoName.trim() !== "" ||
+            newThingsToDoDiscription.trim() !== "" ||
+            newThingsToDoTimeDue.trim() !== "" ||
+            newThingsToDoDone === true
+        ) {
+            setThingsToDo(t => [
+                ...t,
+                {
+                    id: Date.now(),
+                    name: newThingsToDoName,
+                    description: newThingsToDoDiscription,
+                    timeDue: newThingsToDoTimeDue,
+                    done: newThingsToDoDone
+            }
+            ]);
+            setNewThingsToDoName("");
+            setNewThingsToDoDiscription("");
+            setNewThingsToDoTimeDue("");
+            setNewThingsToDoDone(false);
+        }
+    }
+    return(
+       <div className="row">
+        <button className="add-button" onClick={addTask}>
+            Add New Task
+        </button>
+    <input
+            type="text"
+            placeholder="Enter a new Task."
+            value={newThingsToDoName}
+            onChange={handleNameChange}
+        />
+        <input
+            type="text"
+            placeholder="Enter a description."
+            value={newThingsToDoDiscription}
+            onChange={handleDescriptionChange}
+        />
+        <input
+            type="text"
+            placeholder="Enter Time Due" 
+            value={newThingsToDoTimeDue}
+            onChange={handleTimeDueChange}
+        />
+        <input 
+            type="checkbox"   
+                checked={newThingsToDoDone}
+            onChange={e => setNewThingsToDoDone(e.target.checked)}
+        />
+
+                    <ol className="col-12">
+                        {thingsToDo.map((item) => (
+                            <li
+                                key={item.id}
+                                style={{ textDecoration: item.done ? 'line-through' : 'none' }}
+                            >
+                                <h4>{item.name}</h4>
+                                <p>
+                                    {item.description}{" "}--{" "}{item.timeDue}
+                                </p>
+                            </li>
+                        ))}
+                    </ol>
         </div>
     );
 }
-export default ThingsToDo
+
+export default ThingsToDo;
+   
+    
+    
